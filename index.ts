9b2d3806c68e71b0972f398bf693663f7a825c14
fawuzantech <ibrahimpima76@gmail.com>
2025-07-19 15:11:59 -1200

    added verification jwt

--- index.ts
+++ index.ts
@@ -1,14 +1,35 @@
-import jwt from "jsonwebtoken";
+import jwt from "jsonwebtoken"
+import { z } from "zod"
 
-//decode , verify, generate
+const jwtSecret = "secret"
 
-const value = {
-    name: "John",
-    age: 30,
-    city: "New York"
+const emailSchemna = z.string().email()
+const passwordSchema = z.string().min(6)
+
+//generate, decode , verify
+function genToken(username: string, password: string) {
+    const usernameResponse = emailSchemna.safeParse(username)
+    const passwordResponse = passwordSchema.safeParse(password)
+    if(!usernameResponse.success || !passwordResponse){
+      return null
+    }
+    
+const signature = jwt.sign({
+        username,
+        password
+    }, jwtSecret, {expiresIn: "1h"});
+    return signature;
 }
 
-// creating the workbook
-const token = jwt.sign(value, "secret")
+function verifyToken(token:string){
+   const verified = jwt.verify(token, jwtSecret)
+   return verified
+}
 
-console.log(token);
+function decodeToken( token: string) {
+    const decoded= jwt.decode(token)
+    return decoded
+}
+
+const ans = genToken("test", "test")
+
